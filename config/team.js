c40b6c040d72e95292935b54898b46616e407f66
Sami Suliman <sami.eltayeb@gmail.com>
2026-07-21 15:01:34 -0600

    fix: correctly categorize team members and rebuild team page

--- config/team.js
+++ config/team.js
@@ -1,138 +1,155 @@
-module.exports=[
+module.exports = [
   {
     "id": "dr-0",
     "slug": "dr-0",
     "firstName": "Dr",
     "lastName": "Angela Vinturache",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_angela_vinturache/dr_angela_vinturache.jpg"
+    "role": "Specialist",
+    "photo": "assets/healthplus team/dr_angela_vinturache/dr_angela_vinturache.jpg",
+    "category": "Specialists"
   },
   {
     "id": "dr-1",
     "slug": "dr-1",
     "firstName": "Dr",
     "lastName": "Aous Al Shehabi",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_aous_al_shehabi/dr_aous_al_shehabi.jpg"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr_aous_al_shehabi/dr_aous_al_shehabi.jpg",
+    "category": "Physicians"
   },
   {
     "id": "dr-2",
     "slug": "dr-2",
     "firstName": "Dr",
     "lastName": "Asaad Qaddori",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_asaad_qaddori/dr_asaad_qaddori.jpg"
+    "role": "Specialist",
+    "photo": "assets/healthplus team/dr_asaad_qaddori/dr_asaad_qaddori.jpg",
+    "category": "Specialists"
   },
   {
     "id": "dr-3",
     "slug": "dr-3",
     "firstName": "Dr",
     "lastName": "Ayman Habiba",
-    "role": "Physician",
-    "photo": "assets/images/team/placeholder.webp"
+    "role": "Specialist",
+    "photo": "assets/images/team/placeholder.webp",
+    "category": "Specialists"
   },
   {
     "id": "dr-4",
     "slug": "dr-4",
     "firstName": "Dr",
     "lastName": "Bhavisha Ramsamy",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_bhavisha_ramsamy/dr_bhavisha_ramsamy.jpg"
+    "role": "Specialist",
+    "photo": "assets/healthplus team/dr_bhavisha_ramsamy/dr_bhavisha_ramsamy.jpg",
+    "category": "Specialists"
   },
   {
     "id": "dr-5",
     "slug": "dr-5",
     "firstName": "Dr",
     "lastName": "Elhadi Mohammed",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_elhadi_mohammed/dr_elhadi_mohammed.jpg"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr_elhadi_mohammed/dr_elhadi_mohammed.jpg",
+    "category": "Physicians"
   },
   {
     "id": "dr-6",
     "slug": "dr-6",
     "firstName": "Dr",
     "lastName": "Hatim Kheir",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_hatim_kheir/dr_hatim_kheir.jpg"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr_hatim_kheir/dr_hatim_kheir.jpg",
+    "category": "Physicians"
   },
   {
     "id": "dr-7",
     "slug": "dr-7",
     "firstName": "Dr",
     "lastName": "May Aldabbagh",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_may_aldabbagh/dr_may_aldabbagh.jpg"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr_may_aldabbagh/dr_may_aldabbagh.jpg",
+    "category": "Physicians"
   },
   {
     "id": "dr-8",
     "slug": "dr-8",
     "firstName": "Dr",
     "lastName": "Mehwish Hanif",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_mehwish_hanif/dr_mehwish_hanif.png"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr_mehwish_hanif/dr_mehwish_hanif.png",
+    "category": "Physicians"
   },
   {
     "id": "dr-9",
     "slug": "dr-9",
     "firstName": "Dr",
     "lastName": "Nour Bukhres",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_nour_bukhres/dr_nour_bukhres.jpeg"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr_nour_bukhres/dr_nour_bukhres.jpeg",
+    "category": "Physicians"
   },
   {
     "id": "dr-10",
     "slug": "dr-10",
     "firstName": "Dr",
     "lastName": "Oladotun Oyediran",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_oladotun_oyediran/dr_oladotun_oyediran.jpg"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr_oladotun_oyediran/dr_oladotun_oyediran.jpg",
+    "category": "Physicians"
   },
   {
     "id": "dr-11",
     "slug": "dr-11",
     "firstName": "Dr",
     "lastName": "Radhya Aboaziza",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_radhya_aboaziza/dr_radhya_aboaziza.png"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr_radhya_aboaziza/dr_radhya_aboaziza.png",
+    "category": "Physicians"
   },
   {
     "id": "dr-12",
     "slug": "dr-12",
     "firstName": "Dr",
     "lastName": "Sandeep Kumar",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_sandeep_kumar/dr_sandeep_kumar.png"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr_sandeep_kumar/dr_sandeep_kumar.png",
+    "category": "Physicians"
   },
   {
     "id": "dr-13",
     "slug": "dr-13",
     "firstName": "Dr",
     "lastName": "Tariq Khan",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_tariq_khan/dr_tariq_khan.jpeg"
+    "role": "Specialist",
+    "photo": "assets/healthplus team/dr_tariq_khan/dr_tariq_khan.jpeg",
+    "category": "Specialists"
   },
   {
     "id": "dr-14",
     "slug": "dr-14",
     "firstName": "Dr",
     "lastName": "Tyral Ramsamy",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr_tyral_ramsamy/dr_tyral_ramsamy.jpg"
+    "role": "Specialist",
+    "photo": "assets/healthplus team/dr_tyral_ramsamy/dr_tyral_ramsamy.jpg",
+    "category": "Specialists"
   },
   {
     "id": "dr-15",
     "slug": "dr-15",
     "firstName": "Dr",
     "lastName": " Ahmed Enbya",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr__ahmed_enbya/dr__ahmed_enbya.jpeg"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr__ahmed_enbya/dr__ahmed_enbya.jpeg",
+    "category": "Physicians"
   },
   {
     "id": "dr-16",
     "slug": "dr-16",
     "firstName": "Dr",
     "lastName": " Sidra Feroz",
-    "role": "Physician",
-    "photo": "assets/healthplus team/dr__sidra_feroz/dr__sidra_feroz.jpg"
+    "role": "Family Physician",
+    "photo": "assets/healthplus team/dr__sidra_feroz/dr__sidra_feroz.jpg",
+    "category": "Physicians"
   }
-]
+];
